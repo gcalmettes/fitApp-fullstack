d28123df3d00b87a7f0baf141e3101ce1fa2e241
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2018-10-11 12:57:06 -0700

    bug fix: correct total number of traces shown when no traces

--- frontend/src/js/components/NumberTag.js
+++ frontend/src/js/components/NumberTag.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NumberTag = props => {
   const { number, total } = props
   const style = props.style || {}
-  return <h1 className="title" style={Object.assign(style, {'display': 'inline-block'})}> {number || 0}/{total || 0}</h1>
+  return <h1 className="title" style={Object.assign(style, {'display': 'inline-block'})}> {number || 0}/{total >= 0 ? total : 0}</h1>
 }
 
 export default NumberTag
