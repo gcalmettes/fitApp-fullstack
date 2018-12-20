502062ba8d513e237864353a7ed866f448b7edd9
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2018-12-19 17:19:43 -0800

    added single exponential model fit

--- models/fitmethods.py
+++ models/fitmethods.py
@@ -3,8 +3,32 @@ import pandas as pd
 
 
 ########################################
-# Composite Model and initial parameters
+# Fitting models
 ########################################
+
+## Single exponential composite
+## and correspondind initial parameters
+
+def makeSingleExpoModel():
+    constant = lmfit.models.ConstantModel()
+    expoDecay = lmfit.models.ExponentialModel()
+    model = expoDecay + constant
+    return [model, expoDecay, constant]
+
+
+def makeSingleModelParams(model, c=1.4, d1=15., a1=0.5, decreasing=True):
+    params = model.make_params()
+    params['c'].set(c)
+    params['decay'].set(d1, min=0.1, max=80.)
+    if decreasing:
+        params['amplitude'].set(a1, min=0.)
+    else:
+        params['amplitude'].set(a1)
+    return params
+
+
+## Double decreasing exponential composite Model
+## and correspondind initial parameters
 
 def makeDoubleExpoModel(prefix1='exp1_', prefix2='exp2_'):
     constant = lmfit.models.ConstantModel()
@@ -14,7 +38,7 @@ def makeDoubleExpoModel(prefix1='exp1_', prefix2='exp2_'):
     return [model, expoDecay1, expoDecay2, constant]
 
 
-def makeModelParams(model, c=1.4, d1=15., a1=0.5, d2=200, a2=0.5, 
+def makeDoubleModelParams(model, c=1.4, d1=15., a1=0.5, d2=200, a2=0.5, 
                     prefix1='exp1_', prefix2='exp2_'):
     params = model.make_params()
     params['c'].set(c)
@@ -24,12 +48,20 @@ def makeModelParams(model, c=1.4, d1=15., a1=0.5, d2=200, a2=0.5,
     params[f'{prefix2}amplitude'].set(a2, min=0.)
     return params
 
+########################################
+# Fitting routine
+########################################
 
-def fitModel(xData, yData, bounds):
-    # make composite model
-    components = makeDoubleExpoModel()
-    fullModel = components[0]
-    params = makeModelParams(fullModel, c=1.4, d1=15.5, a1=10, d2=200, a2=0.5)
+def fitModel(xData, yData, bounds, model='doubleDecreasingExpo'):
+
+    if model == 'doubleDecreasingExpo':
+        # make composite model
+        components = makeDoubleExpoModel()
+        fullModel = components[0]
+        params = makeDoubleModelParams(fullModel, c=1.4, d1=15.5, a1=10, d2=200, a2=0.5)
+
+    if model == 'singleDecreasingExpo':
+        return
 
     lim1, lim2 = bounds
     mask = (xData >= xData[lim1]) & (xData <= xData[lim2])
