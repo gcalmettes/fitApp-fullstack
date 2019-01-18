e2fb8c084cac36f08a7eb3fbd6a0855e843273cc
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2019-01-18 13:54:56 -0800

    better compartimentalization of the DataAnalyzer

--- app_frontend/src/js/components/DataAnalyzer.js
+++ app_frontend/src/js/components/DataAnalyzer.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import { dataActions } from './../redux/actionTypes';
-
-import classNames from 'classnames';
 
 import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
 
 import { ClippedDrawer, drawerWidth } from './ClippedDrawer'
 import { FileLoader } from './FileLoader'
@@ -12,20 +9,9 @@ import { GraphDataContext } from './GraphDataContext'
 import { GraphDataFocus } from './GraphDataFocus'
 import { NumberTag } from './NumberTag'
 import { GraphFitGridComponents } from './GraphFitGridComponents'
+import { FitOptions } from './FitOptions'
+import { SaveButton } from './SaveButton'
 import { CommentBox } from './CommentBox'
-
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-
-import Divider from '@material-ui/core/Divider';
-import Button from '@material-ui/core/Button';
-import SaveIcon from '@material-ui/icons/Save';
-import ShareIcon from '@material-ui/icons/Share';
-
 
 import { withStyles } from '@material-ui/core/styles';
 import { theme, addToTheme } from './theme';
@@ -41,27 +27,8 @@ const styles = addToTheme(
     drawer:{
       overflow: 'hidden',
     },
-    formControl: {
-      margin: `${theme.spacing.unit*3}px ${theme.spacing.unit*3}px 0 ${theme.spacing.unit*3}px`,
-    },
-    group: {
-      margin: 0,
-      display: 'flex',
-      flexDirection: 'column',
-      flexWrap: 'nowrap',
-      flexShrink: 0,
-    },
     font: {
       fontFamily: 'sans-serif'
-    },
-    button: {
-      margin: theme.spacing.unit*3,
-    },
-    leftIcon: {
-      marginRight: theme.spacing.unit,
-    },
-    iconSmall: {
-      fontSize: 20,
     },
     numberTagComponent: {
       minWidth: '135px',
@@ -73,88 +40,15 @@ const styles = addToTheme(
   }
 )
 
-//////////////////////////////////
-// DRAWER
-//////////////////////////////////
-
- 
-const getFitOptions = opts => opts.map((d,i) => 
-  <FormControlLabel styles={{ 'display': 'block'}} key={`radio${i}`} value={d} control={<Radio color="primary"/>} label={d} />
-)
-
-const fitOptions = [
-  'DbleExponentialDown', 
-  'SingleExponentialUp',
-  'SingleExponentialDown',
-  'Reference'
-]
-
-class DataAnalyzer extends React.Component {
-  constructor(props){
-    super(props)
-
-    this.state = {
-      fitOptionsValue: fitOptions[0],
-    }
-  }
-
-  selectFitOption(event) {
-    this.setState({ fitOptionsValue: event.target.value })
-  }
-
-  sendToFit(){
-    const { data, currentTrace , fitRange, dispatch } = this.props
-    const trace = data[`trace${currentTrace-1}`]
-    if (fitRange) {
-      const [minLim, maxLim] = fitRange
-      const dataToFit = trace.slice(minLim, maxLim+1)
-        .reduce((acc, point) => {
-          acc.x.push(point.x)
-          acc.y.push(point.y)
-          return acc
-        }, {x:[], y: []})
-      dispatch({
-        type: dataActions.SEND_DATA_TO_FIT, 
-        fitSettings: { dataToFit, type: this.state.fitOptionsValue }
-      })
-    }
-  }
-
-  saveData(){
-    const { authentication, analysis, dispatch } = this.props
-    dispatch({
-      type: dataActions.SAVE_DATA_TO_DATABASE, 
-      data: { ...analysis },
-      authentication,
-    })
-  }
-
-  render() {
-    const { classes } = this.props
+export const DataAnalyzer = withStyles(styles)(props => {
+    const { classes } = props
     return (
       <React.Fragment>
         <ClippedDrawer className={classes.drawer}>
-          <FormControl component="fieldset" className={classes.formControl}>
-          <FormLabel component="legend">Fitting Options</FormLabel>
-          <RadioGroup
-            name="fittingOptions"
-            className={classes.group}
-            value={this.state.fitOptionsValue}
-            onChange={this.selectFitOption.bind(this)}
-          >
-            {getFitOptions(fitOptions)}
-          </RadioGroup>
-        </FormControl>
-        <Button variant="contained" className={classes.button} onClick={this.sendToFit.bind(this)}>
-          <ShareIcon className={classNames(classes.leftIcon, classes.iconSmall)} />
-          Fit
-        </Button>
+          <FitOptions />
         <Divider />
         <CommentBox />
-        <Button variant="contained" color="primary" className={classes.button} onClick={this.saveData.bind(this)}>
-          <SaveIcon className={classNames(classes.leftIcon, classes.iconSmall)} />
-          Save
-        </Button>
+        <SaveButton />
         </ClippedDrawer>
         <div className={classes.root}>
           <Grid container spacing={0} className={classes.gridContainer}>
@@ -177,16 +71,4 @@ class DataAnalyzer extends React.Component {
         </div>
       </React.Fragment>
     );
-  }
-}
-
-
-const mapStateToProps = ({ authentication, dataset }) => ({ 
-  currentTrace: dataset.display.currentTrace,
-  data: dataset.metaData.data,
-  fitRange: dataset.analysis.fitRange,
-  analysis: dataset.analysis,
-  authentication,
 })
-const connectedAnalyzer = connect(mapStateToProps)(withStyles(styles)(DataAnalyzer));
-export { connectedAnalyzer as DataAnalyzer }; 
---
+++ app_frontend/src/js/components/SaveButton.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+import { dataActions } from './../redux/actionTypes';
+
+import classNames from 'classnames';
+
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+
+import { withStyles } from '@material-ui/core/styles';
+import { theme, addToTheme } from './theme';
+
+const styles = addToTheme(
+  {
+    button: {
+      margin: theme.spacing.unit*3,
+    },
+    leftIcon: {
+      marginRight: theme.spacing.unit,
+    },
+    iconSmall: {
+      fontSize: 20,
+    },
+  }
+)
+
+class SaveButton extends React.Component {
+  
+  saveData(){
+    const { authentication, metaData, analysis, dispatch } = this.props
+    dispatch({
+      type: dataActions.SAVE_DATA_TO_DATABASE, 
+      data: { ...analysis },
+      authentication,
+    })
+  }
+
+  render(){
+    const { nTraces, currentTrace, classes } = this.props
+
+    return (
+      <Button variant="contained" color="primary" className={classes.button} onClick={this.saveData.bind(this)}>
+        <SaveIcon className={classNames(classes.leftIcon, classes.iconSmall)} />
+        Save
+      </Button>
+    )
+  }
+}
+
+const mapStateToProps = ({ authentication, dataset }) => ({ 
+  authentication,
+  analysis: dataset.analysis,
+  metaData: dataset.metaData
+})
+const connectedSaveButton = connect(mapStateToProps)(withStyles(styles)(SaveButton));
+export { connectedSaveButton as SaveButton }; 
