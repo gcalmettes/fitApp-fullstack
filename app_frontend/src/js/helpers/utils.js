352cf58c73816e1884026dd1e852512eceb9e7ad
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2019-01-25 05:28:04 -0800

    add throttle and debounce functions

--- app_frontend/src/js/helpers/utils.js
+++ app_frontend/src/js/helpers/utils.js
@@ -23,3 +23,26 @@ export const postData = (url = '', data = {}, access_token = null) => {
   })
     .then(response => response.json())
 }
+
+export const throttle = (func, limit) => {
+  let inThrottle
+  return function() {
+    const args = arguments
+    const context = this
+    if (!inThrottle) {
+      func.apply(context, args)
+      inThrottle = true
+      setTimeout(() => inThrottle = false, limit)
+    }
+  }
+}
+
+export const debounce = (func, delay) => {
+  let inDebounce
+  return function() {
+    const context = this
+    const args = arguments
+    clearTimeout(inDebounce)
+    inDebounce = setTimeout(() => func.apply(context, args), delay)
+  }
+}
