007b20e7d6e4904b0523b3ff6b7b2c2b3f54c36f
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2019-01-25 14:54:21 -0800

    remove old version

--- _old/frontend/src/js/components/Title.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Title = props => {
-  const { title } = props
-  return <h1 className="title" style={{'display': 'inline-block'}}> {title}</h1>
-}
-
-export default Title
